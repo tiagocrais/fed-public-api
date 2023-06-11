17ac3711902a252a9d0224be4479597f5232fdf1
Tiago Crais Camargo <tiagocrais@gmail.com>
2023-06-11 13:00:41 -0300

    Criação do component cep e app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CepComponent } from './cep/cep.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CepComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cep/cep.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cep',
+  templateUrl: './cep.component.html',
+  styleUrls: ['./cep.component.css']
+})
+export class CepComponent {
+
+}
