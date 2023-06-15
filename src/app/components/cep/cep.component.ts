d577d2af2bf19ed1ced57abddb98cd44143727ac
Tiago Crais Camargo <tiagocrais@gmail.com>
2023-06-15 19:24:20 -0300

    Implementando requisição, ajustes de layout e criação de estilização

--- src/app/components/cep/cep.component.ts
+++ src/app/components/cep/cep.component.ts
@@ -13,28 +13,23 @@ export class CepComponent implements OnInit{
   endereco!: EnderecoResponse;
 
   constructor(private http: HttpClient) {
-    //this.endereco = new EnderecoResponse("", "", "", "", "", "", "");
    }
 
   ngOnInit(): void {
-    console.log("ngOninit");
   }
 
   consultarCep(): void {
-    console.log(`consultarCep cep = ${this.cep}`);
     if (this.cep.length !== 8) {
-      console.log('CEP inválido!');
       return;
     }
 
-    const apiUrl = `http://localhost:8080/consulta/cep/${this.cep}`;
+    const apiUrl = `/api/consulta/cep/${this.cep}`;
     this.http.get(apiUrl).subscribe(
       (data: any) => {
         this.endereco = data;
-        console.log(`endereco = ${JSON.stringify(this.endereco)}`);
       },
       (error) => {
-        console.log('Erro na requisição:', error);
+        //implementar ação
       }
     );
   }
